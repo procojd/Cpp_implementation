48e14a16f32849e74a122fca2bc43aa38e69cc95
procojd <jatindayal2k19a614@gmail.com>
2024-02-08 11:50:45 +0530

    dp

--- dynamic_programming/LCS.cpp
+++ dynamic_programming/LCS.cpp
@@ -18,7 +18,22 @@ int LCS_mem(vector<vector<int>>& dp, const string& s1, const string& s2, int i, 
         return dp[i][j] = max(LCS_mem(dp, s1, s2, i, j - 1), LCS_mem(dp, s1, s2, i - 1, j));
 }
 
- 
+int LCS_tab(const string& s1, const string& s2) {
+    int m = s1.length();
+    int n = s2.length();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+    return dp[m][n];
+}
 
 int main() {
     string s1 = "AGGTAB";
